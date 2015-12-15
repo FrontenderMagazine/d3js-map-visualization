8f8d28ff14b5718498b1094255507e32fb1062f1
Igor Yeryomin <yeryomin.igor@gmail.com>
2015-12-15 12:14:50 +0200

    Update map colors
    Signed-off-by: Igor Yeryomin <yeryomin.igor@gmail.com>

--- src/js/world.js
+++ src/js/world.js
@@ -1,6 +1,5 @@
 window.onload = function () {
-
-    var svg, colorSet, path, attributeArray = [],
+    var svg, defColor, colorSet, path, attributeArray = [],
         currentAttribute = 0,
         playing = false;
 
@@ -10,22 +9,31 @@ window.onload = function () {
     }
 
     function setMap() {
-        var width = 600,
+        var width = 818,
             height = 600;
 
         svg = d3.select('#map').append('svg')
             .attr('width', width)
             .attr('height', height);
 
-        colorSet = [
-            '#d53e4f',
+        colorsTheGuardian = [
+            "#ca2345",
+            "#ed3d61",
+            "#f58680",
+            "#fdd09e",
+            "#daeac1",
+            "#8ac7cd",
+            "#39a4d8"];
+        colors = [
+            '#990000',
+            '#d7301f',
+            '#ef6548',
             '#fc8d59',
-            '#fee08b',
-            '#ffffbf',
-            '#e6f598',
-            '#99d594',
-            '#3288bd'
-        ];
+            '#fdbb84',
+            '#fdd49e',
+            '#fef0d9',
+            ];
+        defColor = '#a0b5bb';
 
         // получение GeoJSON из TopoJSON (TopoJSON -> GeoJSON)
         // var world = topojson.feature(worldmap, worldmap.objects.world);
@@ -33,7 +41,7 @@ window.onload = function () {
         /* Заготовки различных проекций */
         // Получаем определение одной из проекций
         var mercator = d3.geo.mercator()
-                        .scale(90)
+                        .scale(95)
                         .translate([width / 2, height / 2]);
         // Получаем определение одной из проекций
         var orthographic = d3.geo.orthographic()
@@ -48,9 +56,14 @@ window.onload = function () {
                           .parallels([50, 60])
                           .scale(100)
                           .translate([width / 2, height / 2]);
+        // Получаем определение одной из проекций
+        var miller = d3.geo.miller()
+                        .scale(130)
+                        .translate([width / 2, height / 2])
+                        .precision(.1);
         
         // Определяем генератор пути (path) по конкретной проекции
-        path = d3.geo.path().projection(mercator);
+        path = d3.geo.path().projection(miller);
         
         /* Отрисовка контура всего мира */
         // svg.append("path")
@@ -112,31 +125,35 @@ window.onload = function () {
                 return "code_" + d.id; }, true)
             .attr("d", path);
 
-        var dataRange = getDataRange();
         d3.selectAll('.country')
             .style('fill', function(d) {
-                return getColor(d.properties[attributeArray[currentAttribute]], dataRange);
+                return getColor(d.properties[attributeArray[currentAttribute]]);
             });
     }
 
-    function getDataRange() {
-        return [0,100];
-    }
-
-    function getColor(valueIn, valuesIn) {
-        // var color = d3.scale.linear()
-        //     .domain([valuesIn[0],valuesIn[1]])
-        //     .range([.3,1]);
-
-        var color = d3.scale.linear()
-            .domain([valuesIn[1], valuesIn[0]])
-            .range(['#9e0142', '#abdda4']);
-
-        if (valuesIn[1] >= valueIn >= valuesIn[0] && Number.isInteger(valueIn)) {
-            return color(valueIn);
-        } else {
-            return "#a0b5bb";
+    function getColor(n) {
+        var color = defColor;
+        if ( Number.isInteger(n) ) {
+            if (83 < n && n <= 100) {
+                color = colors[0];
+            } else if (49 < n && n <= 83) {
+                color = colors[1];
+            } else if (41 < n && n <= 49) {
+                color = colors[2];
+            } else if (33 < n && n <= 41) {
+                color = colors[3];
+            } else if (25 < n && n <= 33) {
+                color = colors[4];
+            } else if (17 < n && n <= 25) {
+                color = colors[5];
+            } else if (0 <= n && n <= 17) {
+                color = colors[6];
+            } else {
+                return defColor;
+            }
         }
+
+        return color;
     }
 
     function animateMap() {
@@ -164,11 +181,10 @@ window.onload = function () {
     }
 
     function sequenceMap() {
-        var dataRange = getDataRange();
         d3.selectAll('.country').transition()
             .duration(0)
             .style('fill', function(d) {
-                return getColor(d.properties[attributeArray[currentAttribute]], dataRange);
+                return getColor(d.properties[attributeArray[currentAttribute]]);
             });
     }
 
